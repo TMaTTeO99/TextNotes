ca41dfe49f641ee6799fef07411c9d9ac016e887
Matteo <teo@MattePc.>
2025-01-03 13:08:40 +0100

    start web server

---
+++ TextNotes/src/main/java/com/example/TextNotes/TextNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.TextNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextNotesApplication.class, args);
+	}
+
+}
