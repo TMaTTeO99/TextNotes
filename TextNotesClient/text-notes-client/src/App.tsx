e41d4fc7376787d0df7e968ea748e8f7236903c8
Matteo <teo@MattePc.>
2025-01-06 21:34:51 +0100

    added localstorage's clear between multiple restart of my web app client

--- TextNotesClient/text-notes-client/src/App.tsx
+++ TextNotesClient/text-notes-client/src/App.tsx
@@ -5,7 +5,12 @@ import { MyProvider } from './myComponents/MyContext';
 
 function App() {
   
-  
+  //for all restart of my app i need to clear my localstorage
+  //is used sessionStorage to check if app was restarted or not
+  if(!sessionStorage.getItem("appStarted")){
+    sessionStorage.setItem("appStarted", "true")
+    localStorage.clear();
+  }
   
   return (
 
